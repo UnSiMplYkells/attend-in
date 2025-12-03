45549a1596915f081112f9ba11b93a605cdbafff
__Kells <156234732+UnSiMplYkells@users.noreply.github.com>
2025-12-03 19:23:06 +0100

    restructured the branching

--- PROJECTS/Forever Radiant/src/Features/Cart/EmptyCart.jsx
+++
@@ -1,5 +0,0 @@
-export default function EmptyCart() {
-  return (
-    <div>EmptyCart</div>
-  )
-}
--- PROJECTS/Forever Radiant/src/Features/Reviews/ReviewForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function testimonialForm() {
-  return (
-    <div>testimonialForm</div>
-  )
-}
