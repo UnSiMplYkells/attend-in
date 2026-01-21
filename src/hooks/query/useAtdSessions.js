9ce11c2057920e76ff48e37f50906fc6607123d1
UnSiMpLYkells <nwachukwukells007@gmail.com>
2026-01-21 02:17:09 +0100

    attendance session hook and db calls

---
+++ src/hooks/query/useAtdSessions.js
@@ -0,0 +1,67 @@
+import {
+  setAtdSession as setAtdSessionApi,
+  getActiveAtdSession,
+  getAttendanceSessionByQr
+} from "@/lib/server/atdSession";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import getCurrentTime from "@/app/helper/getCurrentTime";
+import toast from "react-hot-toast";
+
+//sets attendance session with data from button click on the parent element where it was called
+export function useSetAtdSessions() {
+  const queryClient = useQueryClient();
+
+  const { mutate: setAtdSession, isPending: issetAtdSessionLoading } =
+    useMutation({
+      mutationFn: async (variables) => {
+        return await setAtdSessionApi(variables);
+      },
+      onSuccess: (data) => {
+        toast.success("Attendance session started!")
+        queryClient.invalidateQueries({
+          queryKey: ["active-attendance-session"],
+        });
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    });
+
+  return { setAtdSession, issetAtdSessionLoading };
+}
+
+// fetches active attendance session for a given class bassed on compared class id
+export function useGetActiveAtdSession(classIds) {
+  const timeNow = getCurrentTime();
+  const nowNow = new Date().toISOString();
+  const todaysDate = new Date().toISOString().split("T")[0];
+
+  const { data: activeAtdSession, isLoading: isGetAtdSessionLoading } = useQuery({
+      queryKey: ["active-attendance-session", classIds],
+      queryFn: () => getActiveAtdSession({classIds, timeNow, nowNow, todaysDate}),
+      enabled: !!classIds?.length,
+      refetchInterval: 30 * 1000,
+      refetchIntervalInBackground: true,
+    });
+
+  return {
+    activeAtdSession,
+    isGetAtdSessionLoading,
+  };
+}
+
+//fetches attendance session by when a qr code is scanned
+export function useAttendanceSessionByQr(qrData) {
+  const nowISO = new Date().toISOString();
+  const today = new Date().toISOString().split("T")[0];
+
+  const { data: sessionByQr, isLoading: isSessionByQrLoading } = useQuery({
+    queryKey: ["attendance-session-by-Qr", qrData],
+    queryFn: () =>
+      getAttendanceSessionByQr(qrData, nowISO, today),
+    enabled: !!qrData,
+    staleTime: 1000 * 30, // 30 seconds (attendance window is time-based)
+  });
+
+  return { sessionByQr, isSessionByQrLoading };
+}
---
+++ src/lib/server/atdSession.js
@@ -0,0 +1,90 @@
+"use server"
+import { createClient } from "@/app/utils/supabase/server";
+
+export async function setAtdSession({
+  classId,
+  TtLink,
+  sessionData,
+  winStart,
+  winEnd,
+  sessionDate,
+}) {
+  const supabase = await createClient();
+
+  //insert new attendance session
+  const { data, error } = await supabase
+    .from("attendance_sessions")
+    .insert({
+      class_id: classId,
+      timetable_id: TtLink,
+      session_data: sessionData,
+      window_start: winStart,
+      window_end: winEnd,
+      session_date: sessionDate,
+    })
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error(error.message || "Failed to create attendance session");
+  }
+
+  return data;
+}
+
+export async function getActiveAtdSession({ classIds, timeNow, nowNow, todaysDate }) {
+  if (!classIds || classIds.length === 0) return null;
+
+  const supabase = await createClient();
+
+  const ids = Array.isArray(classIds) ? classIds : [classIds];
+
+  //gets active attendance session for the particular user's classes according to todays date
+  const { data: session, error } = await supabase
+    .from("attendance_sessions")
+    .select("*, timetables!inner(*)")
+    .in("class_id", ids)
+    .eq("session_date", todaysDate)
+    .lte("timetables.start_time", timeNow)
+    .gte("timetables.end_time", timeNow)
+    .single()
+
+  if (error) {
+    throw new Error(error.message || "Failed to get attendance session");
+  }
+
+  if (!session) return null;
+
+  //checks if atd taking session is currently active
+  const isActivated =
+    nowNow >= session?.window_start && nowNow <= session?.window_end;
+
+  return {
+    ...session,
+    isActivated,
+  };
+}
+
+export async function getAttendanceSessionByQr(qrData, nowISO, today) {
+  const supabase = await createClient();
+
+  const { data: sessionByQr, error } = await supabase
+    .from("attendance_sessions")
+    .select("*, classes(course_code)")
+    .eq("session_data", qrData)
+    .single();
+
+  if (error) throw error;
+
+  if (!sessionByQr) return null;
+
+  const isActivatedFrmQr =
+    nowISO >= sessionByQr?.window_start &&
+    nowISO <= sessionByQr?.window_end &&
+    sessionByQr?.session_date === today;
+
+  return {
+    ...sessionByQr,
+    isActivatedFrmQr,
+  };
+}
