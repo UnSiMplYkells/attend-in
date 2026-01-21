6ae86f4abcff4093922da45150d6de1fe77c472a
UnSiMpLYkells <nwachukwukells007@gmail.com>
2026-01-21 02:13:02 +0100

    debounce for performance gains

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+//hook to debounce a value, and run a search bar value after 500ms
+export function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
