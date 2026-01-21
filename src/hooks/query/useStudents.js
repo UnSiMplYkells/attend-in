af74c3f2b3eb1495da95020a41d1205a805be2d6
UnSiMpLYkells <nwachukwukells007@gmail.com>
2026-01-21 02:17:54 +0100

    quick  restructuring

--- src/hooks/query/useStudents.js
+++ src/hooks/query/useStudents.js
@@ -2,15 +2,11 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getStudents } from "@/lib/server/students";
 
 export function useGetStudents(department, page = 1, searchTerm = "") {
-  const {
-    data,
-    isLoading: isStudentsLoading,
-    isPlaceholderData,
-  } = useQuery({
+  const {data, isLoading: isStudentsLoading, isPlaceholderData} = useQuery({
     queryKey: ["students", department, page, searchTerm],
     queryFn: () => getStudents(department, page, searchTerm),
     placeholderData: keepPreviousData,
-    staleTime: 5 * 60 * 1000,
+    staleTime: 3 * 60 * 1000,
     enabled: !!department,
   });
 
